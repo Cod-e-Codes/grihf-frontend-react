710500047b0f45ad4a4c527f4ffeb899fbbaeed2
Cody Marsengill <codylmarsengill@gmail.com>
2024-11-30 13:45:21 -0500

    update db.js

--- grihf-frontend-react/server/db.js
+++ grihf-frontend-react/server/db.js
@@ -1,13 +1,13 @@
 const mongoose = require('mongoose');
-const mongoURI =  "mongodb://root:<your-password>@127.0.0.1:27017";
+const mongoURI = "mongodb://root:ld3Un4qRgbf5uYNG6pTRabMS@172.21.69.133:27017";
 
 const connectToMongo = async (retryCount) => {
     const MAX_RETRIES = 3;
     const count = retryCount ?? 0;
     try {
-        await mongoose.connect(mongoURI, { dbName: 'stayhealthybeta1'});
-        console.info('Connected to Mongo Successfully')
-
+        // Connect to MongoDB using mongoose
+        await mongoose.connect(mongoURI, { dbName: 'stayhealthybeta1' });
+        console.info('Connected to MongoDB Successfully');
         return;
     } catch (error) {
         console.error(error);
@@ -18,10 +18,9 @@ const connectToMongo = async (retryCount) => {
             throw new Error('Unable to connect to Mongo!');
         }
 
-        console.info(`Retrying, retry count: ${nextRetryCount}`)
+        console.info(`Retrying, retry count: ${nextRetryCount}`);
 
         return await connectToMongo(nextRetryCount);
-
     }
 };
 
